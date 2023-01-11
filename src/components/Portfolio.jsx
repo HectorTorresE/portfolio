37fd237b0f0b5b9f2dcf871f1e1ec8139e5d656c
HectorTorresE <hectorjte@gmail.com>
2023-01-11 11:42:27 -0600

    Day 1: add intro and mobile intro

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,7 +4,10 @@ import PortfolioItem from "./PortfolioItem";
 
 function Portfolio() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center">
+    <div className="flex flex-col items-center">
+      <div className="">
+        <h1 className="text-orange text-3xl font-bold">My Recent Works</h1>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {portfolio.map((project) => (
           <PortfolioItem
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,20 @@ module.exports = {
   content: ['./index.html','./src/**/*.{jsx,js}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: { 
+        white: '#ffffff',
+        blue: '#091e42',
+        orange: '#f55800',
+        background: '#1c1a19',
+        navbar: '#3e3c3c',
+      }
+    },
+    fontFamily: { 
+      'title': ['Crete Round', 'serif'],
+      'header': ['Roboto', 'sans-serif'],
+      'text': ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [],
 }
